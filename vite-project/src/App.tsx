ff82e2688681207f743fe67901078168ff98ffb1
arvid1212 <145985929+arvid1212@users.noreply.github.com>
2024-02-15 17:17:45 +0100

    changes

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,27 +1,8 @@
 
-import styles from './App.module.css'
-import {useEditor,EditorContent} from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
+import styles from './App.scss'
+
 function App() {
-const editor = useEditor({
-  extensions: [StarterKit],
-  content: "<p>Hello World!<p>",
-
-
-});
-
-
-return(
-    <div className = {styles.pageContainer}> 
-      <div className = {styles.sidebar}> </div>
-            <div className={styles.editorContainer}> 
-            <div className={styles.toolbar}> toolbar</div>
-            <EditorContent editor={editor} />
-      </div>
-
-      </div>
-
-
+return
     
 
 
